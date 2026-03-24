15d20656dfd623bcc797ffa9e6b77bd919b5a9c5
Samuel Tetteh <khobbylynx55@gmail.com>
2026-03-24 18:38:48 +0000

    add error log for debugging

--- diaryapp/settings.py
+++ diaryapp/settings.py
@@ -158,3 +158,33 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'ERROR',  # Change to 'DEBUG' for even more detail
+            'propagate': False,
+        },
+
+    },
+
+}
