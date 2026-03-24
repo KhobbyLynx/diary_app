2c34e4b705dd217aedf9ffa80b39a124f65525d5
Samuel Tetteh <khobbylynx55@gmail.com>
2026-03-24 22:16:24 +0000

    clean up

--- diaryapp/urls.py
+++ diaryapp/urls.py
@@ -16,14 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import fix_db
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('diary.urls')),
-    # Temporary URL to fix the database issue on Render. Remove after use.
-    path('fix-db/', fix_db),
 ]
 
 # Error handlers
