712aab059f4ae289dadccd68b95904dae7bd4284
Samuel Tetteh <khobbylynx55@gmail.com>
2026-03-24 19:24:59 +0000

    run script to create db tables

--- diaryapp/urls.py
+++ diaryapp/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import fix_db
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('diary.urls')),
+    path('fix-my-db/', fix_db),
 ]
 
 # Error handlers
